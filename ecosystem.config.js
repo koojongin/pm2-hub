9f220d737a49af82c58aefe4cbc40cb03334fdc0
koojongin <jiku90@naver.com>
2026-07-30 21:30:54 +0900

    fix: disable origin

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,15 +4,15 @@ module.exports = {
     // because it is built with unreal engine, specifically v5.5
     // 2026/04/07
     apps: [
-        {
-            name: '7dso',
-            cwd: 'C:/projects/7dso-db',
-            script: 'node_modules/next/dist/bin/next',
-            args: 'start -p 3001 -H 0.0.0.0',
-            watch: false,
-            exec_mode: 'fork',
-            interpreter: 'node'
-        },
+        // {
+        //     name: '7dso',
+        //     cwd: 'C:/projects/7dso-db',
+        //     script: 'node_modules/next/dist/bin/next',
+        //     args: 'start -p 3001 -H 0.0.0.0',
+        //     watch: false,
+        //     exec_mode: 'fork',
+        //     interpreter: 'node'
+        // },
         {
             name: 'sd',
             cwd: 'C:/projects/stardive-db',
