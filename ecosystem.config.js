99f1b1b3d69a3ed658f0cde8e9617b9905bc632f
koojongin <jiku90@naver.com>
2026-08-20 23:52:05 +0900

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,9 +25,9 @@ module.exports = {
         {
             name: 'entaria',
             cwd: 'C:/projects/entaria',
-            script: 'npm',
-            args: 'run start', // package.json의 "start" 스크립트 실행
-            instances: 1,      // 👈 인스턴스 1개로 명시적 고정
+            script: 'node_modules/tsx/dist/cli.mjs', // 👈 tsx 실행 파일을 직접 지정
+            args: 'server.ts',
+            instances: 1, // 👈 1개로 고정
             exec_mode: 'fork',
             watch: false,
             env: {
